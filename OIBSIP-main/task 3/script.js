1e46dea2146e60b18373a1c397b34068dedc44a5
Ashutosh upadhyay <77200415+iamashu222122@users.noreply.github.com>
2022-10-21 22:47:25 +0530

    commit

---
+++ OIBSIP-main/task 3/script.js
@@ -0,0 +1,21 @@
+let celsius = document.getElementById("celsius");
+let farenheit = document.getElementById("farenheit");
+
+
+celsius.oninput = () => {
+    let output = (parseFloat(celsius.value) * 9) / 5 + 32;
+    farenheit.value = parseFloat(output.toFixed(2));
+
+};
+
+farenheit.oninput = () => {
+    let output = (parseFloat(farenheit.value) - 32) * 5 / 9;
+    celsius.value = parseFloat(output.toFixed(2));
+
+};
+kelvin.oninput = () =>
+{
+    let output = (parseFloat(kelvin.value) - 32) * 5 / 9;
+    celsius.value = parseFloat(output.toFixed(2));
+}
+console.log("hi");
